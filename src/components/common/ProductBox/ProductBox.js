1fc80d92bf5345597d3e50050d5ce3343e0ecbde
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-06-17 22:49:31 +0200

    InitalState added

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import styles from './ProductBox.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingBasket} from '@fortawesome/free-solid-svg-icons';
+import Button from '../Button/Button';
 
 class ProductBox extends React.Component {
   render() {
@@ -12,7 +15,12 @@ class ProductBox extends React.Component {
           <h5>CONTENT</h5>
         </div>
         <div className={styles.action}>
-          ACTION
+          <Button className={styles.priceBtn} noHover variant='small'>
+              $ 50
+          </Button>
+          <Button className={styles.options} variant='small'>
+            <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
+          </Button>
         </div>
       </div>
     );
