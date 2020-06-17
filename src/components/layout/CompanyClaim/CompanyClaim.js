1ccf91310d0b473ae48dbb3ea515716bc86968aa
Pawel Jurecki <jurecki.pawel@gmail.com>
2020-06-17 10:04:58 +0200

    CompanyClaim Component added

--- src/components/layout/CompanyClaim/CompanyClaim.js
+++ src/components/layout/CompanyClaim/CompanyClaim.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import styles from './CompanyClaim.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneAlt, faStore, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const CompanyClaim = () => (
-  <div>
-    CompanyClaim
+  <div className={styles.root}>
+    <div className='container'>
+      <div className='row align-items-center'>
+        <div className={`col-12 order-3 text-right col-md-4 text-md-left order-md-1 ${styles.phoneNumber}`}>
+          <p>
+            <FontAwesomeIcon className={styles.icon} icon={faPhoneAlt} />
+            +48 606-606-606
+          </p>
+        </div>
+        <div className={`col-6 col-md-3 text-md-center order-md-2 ${styles.logo}`}>
+          <FontAwesomeIcon className={styles.icon} icon={faStore} />
+        </div>
+        <div className={`col text-right order-md-3 ${styles.cart}`}>
+          <a href='#' className={styles.cartBox}>
+            <div className={styles.cartIcon}>
+              <FontAwesomeIcon className={styles.icon} icon={faShoppingCart} />
+            </div>
+            <div className={styles.cartCounter}>123</div>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
